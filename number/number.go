2e0853421609d92ce00e1ce6cd59abb6fb37541f
Mark Bradley <braddle@gmail.com>
2017-03-09 17:26:50 +0000

    Minus Function
    Testing and implementing a function to minus one number from another and return the result as a string.

--- number/number.go
+++ number/number.go
@@ -2,13 +2,27 @@ package number
 
 import "fmt"
 
-// Add function add together the numbers given and returns the result as a string
+// Add function add together the numbers given and returns the result as a
+// string
 func Add(a, b int) string {
 	num := a + b
 
 	switch num {
 	case 4:
 		return "four"
+	default:
+		return fmt.Sprintf("Cannot convert %v to a string", num)
+	}
+}
+
+// Minus function take the value of b from the value of a and return the result
+// as a string
+func Minus(a, b int) string {
+	num := a - b
+
+	switch num {
+	case 3:
+		return "three"
 	default:
 		return fmt.Sprintf("Cannot convert %v to a string", num)
 	}
--- number/number_test.go
+++ number/number_test.go
@@ -20,11 +20,25 @@ func TestSixAndFiveReturnsNumberNotConvertableString(t *testing.T) {
 	assertEquals(t, exp, act)
 }
 
+func TestSixMinusThreeReturnsStringThree(t *testing.T) {
+	act := number.Minus(6, 3)
+	exp := "three"
+
+	assertEquals(t, exp, act)
+}
+
+func TestThreeMinusSixReturnsNumberNotConvertableString(t *testing.T) {
+	act := number.Minus(3, 6)
+	exp := "Cannot convert -3 to a string"
+
+	assertEquals(t, exp, act)
+
+}
+
 func assertEquals(t *testing.T, exp, act string) {
 	if act != exp {
 		t.Error("Actual value did not match Expected value")
 		t.Logf("Expected: %s", exp)
 		t.Logf("Actual:   %s", act)
 	}
-
 }
